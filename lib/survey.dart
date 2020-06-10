8c0f0135ae88d9edb08f89d3a8bfc06892c6f000
dzakwandp <diegodzakwan@gmail.com>
2020-06-10 17:44:23 +0700

    radiobutton

--- lib/survey.dart
+++ lib/survey.dart
@@ -9,44 +9,67 @@ class Survey extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: const Text('Surveys'),
-        leading: IconButton(icon: const Icon(Icons.navigate_before), onPressed: (){
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>HomePage()),);
-        }),),
-        body: MyStatelessWidget(),
+        appBar: AppBar(
+          title: const Text('Surveys'),
+          leading: IconButton(
+              icon: const Icon(Icons.navigate_before),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => HomePage()),
+                );
+              }),
+        ),
+        body: RadioApp(),
       ),
     );
   }
 }
 
 /// This is the stateless widget that the main application instantiates.
-class MyStatelessWidget extends StatelessWidget {
-  MyStatelessWidget({Key key}) : super(key: key);
+enum Pilihan { a, b, c, d, e, f, g, h, i, j, k, l, m, o, p, q, r, s, t, u}
+class RadioApp extends StatefulWidget {
+  RadioApp({Key key}) : super(key: key);
 
   @override
+  _RadioAppState createState() => _RadioAppState();
+}
+
+class _RadioAppState extends State<RadioApp> {
+  Pilihan _character = Pilihan.a;
+
   Widget build(BuildContext context) {
     return Center(
       child: Card(
         child: Column(
-          mainAxisSize: MainAxisSize.min,
+          mainAxisSize: MainAxisSize.max,
           children: <Widget>[
             const ListTile(
-              leading: Icon(Icons.album),
-              title: Text('The Enchanted Nightingale'),
-              subtitle: Text('Music by Julie Gable. Lyrics by Sidney Stein.'),
+              leading: Icon(Icons.question_answer),
+              title: Text('Silahkan jawab pertanyaan dibawah ini sesuai dengan kondisi anda.'),
+              subtitle: Text('Data tidak akan kami sebar luaskan'),
             ),
-            ButtonBar(
-              children: <Widget>[
-                FlatButton(
-                  child: const Text('BUY TICKETS'),
-                  onPressed: () {/* ... */},
-                ),
-                FlatButton(
-                  child: const Text('LISTEN'),
-                  onPressed: () {/* ... */},
-                ),
-              ],
+            const ListTile(
+              title: Text('1. Apakah anda memiliki riwayat demam selama 2 minggu terakhir?'),
             ),
+            ListTile(
+              title: Text('Iya'),
+              leading: Radio(value: Pilihan.a, 
+              groupValue: _character, 
+              onChanged: (Pilihan value){
+                setState(() {
+                _character = value;
+              });
+              })),
+              ListTile(
+              title: Text('Tidak'),
+              leading: Radio(value: Pilihan.b, 
+              groupValue: _character, 
+              onChanged: (Pilihan value){
+                setState(() {
+                _character = value;
+              });
+              }))
           ],
         ),
       ),
