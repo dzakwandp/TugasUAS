c895ea5445b9679a7d74b49e9e483b0a927ce7bc
Digisata <hnaufal123@gmail.com>
2020-06-12 22:30:56 +0700

    Clean the code and create survey

--- lib/survey.dart
+++ lib/survey.dart
@@ -1,92 +1,168 @@
-//import 'dart:js';
-
-import 'homeview.dart';
 import 'package:flutter/material.dart';
 
-class Survey extends StatelessWidget {
+const questions = [
+  'Apakah Anda memiliki riwayat demam selama 2 minggu terakhir?',
+  'Apakah Anda memiliki riwayat batuk/pilek selama 2 minggu terakhir?',
+  'Apakah Anda memiliki riwayat nyeri pada tenggorokan 2 minggu terakhir?',
+  'Apakah Anda memiliki riwayat sesak nafas selama 2 minggu terakhir?',
+  'Apakah Anda memiliki riwayat perjalanan ke luar negeri dalam waktu 2 minggu terakhir?',
+  'Apakah Anda memiliki riwayat perjalanan ke luar kota dalam waktu 2 minggu terakhir?',
+  'Apakah Anda memiliki riwayat kontak erat dengan pasien kasus positif COVID-19?',
+  'Apakah Anda memiliki riwayat mengunjungi wilayah industri yang terkena zona merah COVID-19?',
+  'Apakah Anda memiliki riwayat mengunjungi fasilitas kesehatan yang terkena zona merah COVID-19?',
+  'Apakah Anda memiliki riwayat suhu tubuh melebihi 38 derajat celcius dalam 2 minggu terakhir?',
+];
+
+enum Options { Iya, Tidak, None }
+
+Map<String, Options> answer = {};
+
+void initializeAnswer() {
+  for (int i = 1; i <= questions.length; i++) {
+    answer['answer$i'] = Options.None;
+  }
+}
+
+Options option1;
+
+class Survey extends StatefulWidget {
+  @override
+  _SurveyState createState() => _SurveyState();
+}
+
+class _SurveyState extends State<Survey> {
+  @override
+  void initState() {
+    super.initState();
+    initializeAnswer();
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Surveys'),
-          leading: IconButton(
-            icon: const Icon(Icons.navigate_before),
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => HomePage()),
-              );
-            },
+          backgroundColor: Colors.cyan,
+          centerTitle: true,
+          elevation: 10.0,
+          title: Text(
+            'Survey',
+            style: TextStyle(
+              fontSize: 25.0,
+              fontWeight: FontWeight.bold,
+              fontStyle: FontStyle.normal,
+              color: Colors.white,
+            ),
           ),
         ),
-        body: RadioApp(),
-      ),
-    );
-  }
-}
-
-/// This is the stateless widget that the main application instantiates.
-enum Pilihan { a, b, c, d, e, f, g, h, i, j, k, l, m, o, p, q, r, s, t, u }
-
-class RadioApp extends StatefulWidget {
-  RadioApp({Key key}) : super(key: key);
-
-  @override
-  _RadioAppState createState() => _RadioAppState();
-}
-
-class _RadioAppState extends State<RadioApp> {
-  Pilihan _character = Pilihan.a;
-
-  Widget build(BuildContext context) {
-    return Center(
-      child: Card(
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          children: <Widget>[
-            const ListTile(
-              leading: Icon(Icons.question_answer),
-              title: Text(
-                  'Silahkan jawab pertanyaan dibawah ini sesuai dengan kondisi anda.'),
-              subtitle: Text('Data tidak akan kami sebar luaskan'),
-            ),
-            const ListTile(
-              title: Text(
-                  '1. Apakah anda memiliki riwayat demam selama 2 minggu terakhir?'),
-            ),
-            ListTile(
-              title: Text('Iya'),
-              leading: Radio(
-                value: Pilihan.a,
-                groupValue: _character,
-                onChanged: (Pilihan value) {
-                  setState(
-                    () {
-                      _character = value;
-                    },
-                  );
-                },
-              ),
-            ),
-            ListTile(
-              title: Text('Tidak'),
-              leading: Radio(
-                value: Pilihan.b,
-                groupValue: _character,
-                onChanged: (Pilihan value) {
-                  setState(
-                    () {
-                      _character = value;
-                    },
-                  );
-                },
-              ),
-            ),
-          ],
+        body: _myListView(context),
+        floatingActionButton: FloatingActionButton.extended(
+          onPressed: () {
+            setState(() {
+              calculateResult(context);
+            });
+          },
+          label: Text('Submit'),
+          icon: Icon(Icons.thumb_up),
+          backgroundColor: Colors.cyan,
+          elevation: 5.0,
+          tooltip: 'Submit untuk melihat hasilnya',
         ),
       ),
     );
   }
+
+  Widget _myListView(BuildContext context) {
+    return ListView.builder(
+      itemCount: questions.length,
+      itemBuilder: (context, index) {
+        return Card(
+          margin: EdgeInsets.fromLTRB(10.0, 10.0, 10.0, 5.0),
+          elevation: 5.0,
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text(
+                  '${index + 1}. ${questions[index]}',
+                  style: TextStyle(
+                    fontSize: 15.0,
+                    fontWeight: FontWeight.bold,
+                    fontStyle: FontStyle.normal,
+                    color: Colors.grey[800],
+                  ),
+                ),
+              ),
+              ListTile(
+                title: const Text('Iya'),
+                leading: Radio(
+                  value: Options.Iya,
+                  groupValue: answer['answer${index + 1}'],
+                  onChanged: (Options value) {
+                    setState(
+                      () {
+                        answer['answer${index + 1}'] = value;
+                      },
+                    );
+                  },
+                ),
+              ),
+              ListTile(
+                title: const Text('Tidak'),
+                leading: Radio(
+                  value: Options.Tidak,
+                  groupValue: answer['answer${index + 1}'],
+                  onChanged: (Options value) {
+                    setState(
+                      () {
+                        answer['answer${index + 1}'] = value;
+                      },
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
+
+void calculateResult(BuildContext context) {
+  int _percentage = 0;
+  bool _isValid = false;
+  for (final value in answer.values) {
+    if (value == Options.None) {
+      _isValid = false;
+      break;
+    } else if (value == Options.Iya) {
+      _percentage += 10;
+      _isValid = true;
+    }
+  }
+
+  _isValid
+      ? showAlertDialog(context, 'Hasil Survey',
+          'Persentase terjangkit COVID-19: $_percentage%')
+      : showAlertDialog(
+          context, 'Perhatian!', 'Silahkan isi semua survey terlebih dahulu!!!');
+}
+
+void showAlertDialog(BuildContext context, String title, String content) {
+  showDialog(
+    context: context,
+    builder: (_) => new AlertDialog(
+      title: new Text(title),
+      content: new Text(content),
+      actions: <Widget>[
+        FlatButton(
+          child: Text('Tutup!'),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ],
+    ),
+  );
 }
